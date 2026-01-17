6c848c43fda387b6b76f6f10b63ddfe33f880cd8
Quentin Burgniard <quentinburgniard@gmail.com>
2026-01-17 11:53:08 +0100

    fix: env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiBaseUrl: 'https://api.preview.digitalleman.com/v5',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,3 @@
 export const environment = {
-  apiBaseUrl: '/v5',
-  httpOptions: {
-    headers: {
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzY4MjQ4NjY0LCJleHAiOjE3Njg4NTM0NjR9.spDgkFKMnSOXFEB-aqv2E673NTsBsVggcFuGbm29ac8',
-    },
-  },
+  apiBaseUrl: '/api',
 };
