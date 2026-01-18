34dbdf9f182d8de0375ec5e1d4800c15dd732b73
Quentin Burgniard <quentinburgniard@gmail.com>
2026-01-18 15:26:18 +0100

    feat: add interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { httpErrorInterceptor } from './http-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(),
-    provideRouter(routes)
-  ]
+    provideHttpClient(withInterceptors([httpErrorInterceptor])),
+    provideRouter(routes),
+  ],
 };
